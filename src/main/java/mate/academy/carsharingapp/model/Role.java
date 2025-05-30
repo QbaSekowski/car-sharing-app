beffcdc72ebae853c3f6d04f8faf6b9ba4801829
Jakub Sekowski <sekowskijakub@gmail.com>
2025-05-30 09:52:49 +0200

    Roles applied.

--- src/main/java/mate/academy/carsharingapp/model/Role.java
+++ src/main/java/mate/academy/carsharingapp/model/Role.java
@@ -1,4 +1,33 @@
 package mate.academy.carsharingapp.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", unique = true, nullable = false)
+    private RoleName name;
+
+    public enum RoleName {
+        MANAGER,
+        CUSTOMER
+    }
 }
