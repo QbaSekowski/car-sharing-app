52266e123ad337e71ef953820396a48108c07161
Jakub Sekowski <sekowskijakub@gmail.com>
2025-06-16 08:48:29 +0200

    User and Role repositories added.

---
+++ src/main/java/mate/academy/carsharingapp/repository/car/UserRepository.java
@@ -0,0 +1,9 @@
+package mate.academy.carsharingapp.repository.car;
+
+import mate.academy.carsharingapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
