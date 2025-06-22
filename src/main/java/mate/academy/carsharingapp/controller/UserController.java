9d2915c393c8093add914c43d0436f8fcd0673a1
Jakub Sekowski <sekowskijakub@gmail.com>
2025-06-22 09:52:03 +0200

    Basic for updateUserRole method added.

--- src/main/java/mate/academy/carsharingapp/controller/UserController.java
+++ src/main/java/mate/academy/carsharingapp/controller/UserController.java
@@ -1,7 +1,13 @@
 package mate.academy.carsharingapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.carsharingapp.dto.user.UserRoleUpdateDto;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
         description = "Endpoints for managing users")
 public class UserController {
 
+    @PutMapping("/{id}/roles")
+    @Operation(summary = "Update user's role",
+            description = "Update user's role")
+    public void updateUserRole(@PathVariable Long id,
+                               @RequestBody @Valid UserRoleUpdateDto userRoleUpdateDto) {
+        //userService.updateUserRole(id, userRoleUpdateDto);
+    }
 }
