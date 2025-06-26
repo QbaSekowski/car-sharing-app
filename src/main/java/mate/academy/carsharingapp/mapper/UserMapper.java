d6b91aeb15a6f51c891144de1f8c8c4a657f5834
Jakub Sekowski <sekowskijakub@gmail.com>
2025-06-26 10:09:54 +0200

    UserMapper added.

---
+++ src/main/java/mate/academy/carsharingapp/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package mate.academy.carsharingapp.mapper;
+
+import mate.academy.carsharingapp.dto.user.UserRegistrationRequestDto;
+import mate.academy.carsharingapp.dto.user.UserResponseDto;
+import mate.academy.carsharingapp.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.MappingTarget;
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+    UserResponseDto toDto(User user);
+
+    User toModel(UserRegistrationRequestDto userRegistrationRequestDto);
+
+    User updateUser(UserRegistrationRequestDto userRegistrationRequestDto,
+                    @MappingTarget User user);
+}
