7aef0da9d1ad4a3338b3213ae88e248636f9206c
Jakub Sekowski <sekowskijakub@gmail.com>
2025-07-01 10:42:20 +0200

    Applying GrantedAuthority implementation in Role model.

--- src/main/java/mate/academy/carsharingapp/model/Role.java
+++ src/main/java/mate/academy/carsharingapp/model/Role.java
@@ -11,13 +11,14 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -25,6 +26,11 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(name = "name", unique = true, nullable = false)
     private RoleName name;
+
+    @Override
+    public String getAuthority() {
+        return name.name();
+    }
 
     public enum RoleName {
         MANAGER,
