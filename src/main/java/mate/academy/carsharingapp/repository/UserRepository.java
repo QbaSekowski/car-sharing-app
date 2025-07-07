7afecdf1e514971fed7d93e0d83ade7fbc4f2589
Jakub Sekowski <sekowskijakub@gmail.com>
2025-07-07 15:06:11 +0200

    CustomUserDetailsService applied.

--- src/main/java/mate/academy/carsharingapp/repository/UserRepository.java
+++ src/main/java/mate/academy/carsharingapp/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package mate.academy.carsharingapp.repository;
 
+import java.util.Optional;
 import mate.academy.carsharingapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
