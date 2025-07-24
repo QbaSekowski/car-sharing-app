ad21529015723d326e31b7e36b619f6b3e6f594f
Jakub Sekowski <sekowskijakub@gmail.com>
2025-07-24 12:45:16 +0200

    AuthenticationController applied.

--- src/main/java/mate/academy/carsharingapp/controller/AuthenticationController.java
+++ src/main/java/mate/academy/carsharingapp/controller/AuthenticationController.java
@@ -1,6 +1,8 @@
 package mate.academy.carsharingapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.carsharingapp.dto.user.UserLoginRequestDto;
 import mate.academy.carsharingapp.dto.user.UserLoginResponseDto;
@@ -9,7 +11,11 @@ import mate.academy.carsharingapp.dto.user.UserResponseDto;
 import mate.academy.carsharingapp.exception.RegistrationException;
 import mate.academy.carsharingapp.security.AuthenticationService;
 import mate.academy.carsharingapp.service.user.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,12 +27,21 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserService userService;
 
-    public UserResponseDto register(UserRegistrationRequestDto userRegistrationRequestDto)
+    @PostMapping("/registration")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new user",
+            description = "Create a new user with given username, "
+                    + " password, email, first name, last name")
+    public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto userRegistrationRequestDto)
             throws RegistrationException {
-
+        return userService.register(userRegistrationRequestDto);
     }
 
-    public UserLoginResponseDto login(UserLoginRequestDto userLoginRequestDto) {
-
+    @PostMapping("/login")
+    @Operation(summary = "Log in the user",
+            description = "Log registered user into the service "
+                    + "using email and password")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto userLoginRequestDto) {
+        return authenticationService.authenticate(userLoginRequestDto);
     }
 }
